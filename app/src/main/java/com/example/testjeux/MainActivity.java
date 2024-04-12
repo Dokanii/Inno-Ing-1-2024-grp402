6564efc613cd4bf65ed1774c255c9876c54902a0
Dokanii <tenma.ldpc@gmail.com>
2024-04-12 12:49:09 +0200

    Init page menu

--- app/src/main/java/com/example/testjeux/MainActivity.java
+++ app/src/main/java/com/example/testjeux/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.testjeux;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Trouvez le bouton dans la mise en page de l'activité
+        Button button = findViewById(R.id.button);
+
+        // Définissez un écouteur d'événements pour le bouton
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lorsque le bouton est cliqué, lancez l'activité TriangleActivity
+                Intent intent = new Intent(MainActivity.this, TriangleActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
