172b4b5b5c8996cf23efe77533cb03f433031316
Dokanii <tenma.ldpc@gmail.com>
2024-04-30 22:29:43 +0200

    Suppression de la bande de texte en haut

--- app/src/main/java/com/example/testjeux/TriangleActivity.java
+++ app/src/main/java/com/example/testjeux/TriangleActivity.java
@@ -9,5 +9,7 @@ public class TriangleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_triangle);
+
+        getSupportActionBar().hide();
     }
 }
